bc81285e278151dfb75bdbe986e0f47c438cb18b
ShekuDeen <124774601+ShekuDeen@users.noreply.github.com>
2026-02-18 09:33:02 +0000

    importing model and libarries

---
+++ BIO670SVM.py
@@ -0,0 +1,6 @@
+# Import the necessary models and library
+import numpy as np 
+import tkinter as tk
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score 
