feb8e9062fd8e912d40fcd7fea5ba8bb740fbde5
ShekuDeen <124774601+ShekuDeen@users.noreply.github.com>
2026-02-23 09:27:00 +0000

    USING KMER INSTEAD OF ONE HOT CODE

--- BIO670SVM.py
+++ BIO670SVM.py
@@ -3,9 +3,10 @@ import numpy as np
 import streamlit as st
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score 
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from itertools import product
 
 # Function that Read Fasta file
 def read_fasta_file(file_path):
@@ -25,28 +26,46 @@ def read_fasta_file(file_path):
 
     return sequences
 
+# Check for Open Reading Frames (ORFs)
+def has_orf(sequence, min_length=90):
+    stop_codons = {"TAA", "TAG", "TGA"}
+    
+    # Check all 3 reading frames
+    for frame in range(3):
+        for i in range(frame, len(sequence) - 2, 3):
+            if sequence[i:i+3] == "ATG":
+                for j in range(i+3, len(sequence) - 2, 3):
+                    if sequence[j:j+3] in stop_codons:
+                        orf_length = j - i
+                        if orf_length >= min_length:
+                            return True
+                        break
+    return False
+
+# Generate k-mer features
+def k_mers(sequence, k=3):
+    return [''.join(p) for p in product('ACGT', repeat=k)]
+
+ALL_K_MERS = k_mers('ACGT', k=3)
+
+def k_mer_features(sequence, k=3):
+    counts = dict.fromkeys(ALL_K_MERS, 0)
+    for i in range(len(sequence) - k + 1):
+        k_mer = sequence[i:i+k]
+        if k_mer in counts:
+            counts[k_mer] += 1
+
+    total = sum(counts.values())
+    if total > 0:
+        for k_mer in counts:
+            counts[k_mer] /= total
+
+    return list(counts.values())
+
 # Global variable to store the trained model
 svm_model = None
 scaler = None
 pca = None
-# One-hot encoding of sequences
-def one_hot_encode(sequences, max_length = 250):
-    encoding = {
-        'A': [1, 0, 0, 0],
-        'C': [0, 1, 0, 0],
-        'G': [0, 0, 1, 0],
-        'T': [0, 0, 0, 1],
-    }
-    encoded_sequences = []
-    for nucleotide in sequences:
-        encoded_sequences.extend(encoding.get(nucleotide, [0, 0, 0, 0]))
-
-    required_length = max_length * 4
-    if len(encoded_sequences) < required_length:
-        encoded_sequences += [0] * (required_length - len(encoded_sequences))
-    else:            
-        encoded_sequences = encoded_sequences[:required_length]
-    return encoded_sequences
 
 # Load and train the model
 def train_model():
@@ -59,44 +78,58 @@ def train_model():
     sequences = coding_seq + non_coding_seq
     labels = [1] * len(coding_seq) + [0] * len(non_coding_seq)
 
-    features = np.array([one_hot_encode(seq) for seq in sequences]) 
+    features = np.array([k_mer_features(seq) for seq in sequences]) 
+
+    # Split the Dataset into Training and Testing sets
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=27, stratify=labels)
 
     # Scale the Dataset
     scaler = StandardScaler()
-    features_scaled = scaler.fit_transform(features) 
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test) 
 
     # Dimensionality Reduction using PCA
-    pca = PCA(n_components=100) # reduce to 100 dimensions
+    pca = PCA(n_components=32) # reduce to 32 dimensions
+    X_train_pca = pca.fit_transform(X_train_scaled)
+    X_test_pca = pca.transform(X_test_scaled)
+    
+    #Training Linear SVM model
+    svm_model_linear = SVC(kernel='linear', C=1, gamma='scale', probability=True)
+    svm_model_linear.fit(X_train_pca, y_train)
+    y_pred_linear = svm_model_linear.predict(X_test_pca)
+    accuracy_linear = accuracy_score(y_test, y_pred_linear)
+    print("Linear SVM Model Accuracy:", accuracy_linear)
 
-    features_pca = pca.fit_transform(features_scaled)   
+    # Train the rbf SVM model
+    svm_model_rbf = SVC(kernel='rbf', C=1, gamma='scale', probability=True)
+    svm_model_rbf.fit(X_train_pca, y_train)
 
-    print("Original features shape:", features.shape[1])
-    print("Reduced features shape after PCA:", features_pca.shape[1])
+    y_pred = svm_model_rbf.predict(X_test_pca)
+    accuracy_rbf = accuracy_score(y_test, y_pred)
+    print("RBF SVM Model Accuracy:", accuracy_rbf)
 
+    Conf_mat = confusion_matrix(y_test, y_pred)
 
-    # Split the Dataset into Training and Testing sets  
-    X_train, X_test, y_train, y_test = train_test_split(features_pca, labels, test_size=0.2, random_state=27, stratify=labels)
-
-    # Train the SVM model
-    svm_model = SVC(kernel='rbf', C=1, gamma='scale', probability=True)
-    svm_model.fit(X_train, y_train)
-
-    y_pred = svm_model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("SVM Model Accuracy:", accuracy)
-
-    return svm_model, scaler, pca, accuracy 
+    return svm_model_linear,svm_model_rbf, scaler, pca, accuracy_rbf, accuracy_linear, Conf_mat
 
 # Create a simple GUI using Steamlit
 st.title("🧬 DNA Coding vs Non-Coding Classifier")
 
+svm_model_linear, svm_model_rbf, scaler, pca, accuracy_rbf, accuracy_linear, Conf_mat = train_model()
+
+st.subheader("Model Performance")
+st.write(f"Linear SVM Accuracy: {accuracy_linear:.4f}")
+st.write(f"RBF SVM Accuracy: {accuracy_rbf:.4f}")
 st.write("This model uses SVM + PCA to classify DNA sequences.")
 
-svm_model, scaler, pca, accuracy = train_model()
+st.subheader("Confusion Matrix")
+st.write(Conf_mat)
 
-st.success(f"Model Accuracy: {accuracy:.4f}")
+st.subheader("PCA Explained Variance Ratio")
+st.write(f"Total Variance Retained: {np.sum(pca.explained_variance_ratio_):.4f}")
 
-sequence_input = st.text_area("Enter DNA Sequence (A, C, G, T only):")
+
+sequence_input = st.text_area("Please, Enter DNA Sequence (A, C, G, T only):")
 
 if st.button("Classify"):
     sequence_input = sequence_input.strip().upper()
@@ -105,15 +138,19 @@ if st.button("Classify"):
         st.warning("Please enter a DNA sequence.")
     
     elif not all(nuc in "ACGT" for nuc in sequence_input):
-        st.error("Invalid sequence! Only A, C, G, T are allowed.")
+        st.error("Invalid characters entered! Only A, C, G, T are allowed.")
     
-    else:
-        encoded = np.array(one_hot_encode(sequence_input)).reshape(1, -1)
-        encoded_scaled = scaler.transform(encoded)
-        encoded_pca = pca.transform(encoded_scaled)
+    elif not has_orf(sequence_input):
+        st.warning("No Open Reading Frame (ORF) detected! The sequence is likely non-coding.")
 
-        result = svm_model.predict(encoded_pca)[0]
-        probability = svm_model.predict_proba(encoded_pca)[0][result]
+
+    else:
+        features = np.array(k_mer_features(sequence_input)).reshape(1, -1)
+        features_scaled = scaler.transform(features)
+        features_pca = pca.transform(features_scaled)
+
+        result = svm_model_rbf.predict(features_pca)[0]
+        probability = svm_model_rbf.predict_proba(features_pca)[0][result]
 
         if result == 1:
             st.success("Prediction: Coding")
