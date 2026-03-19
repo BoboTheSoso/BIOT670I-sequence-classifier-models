c6cae67c61e0f7a3e4d2c1d218322269017c5aa0
Solenn Orozco <solinethira@gmail.com>
2026-03-19 19:07:46 -0400

    Enhance script with GUI and DNA feature extraction
    Added a GUI and implemented DNA feature extraction function.

--- Python Scripts/06_Run_scripts.py
+++ Python Scripts/06_Run_scripts.py
@@ -4,9 +4,39 @@ Add a GUI
 '''
 
 import subprocess
+import numpy as np
+from pathlib import Path
+import tkinter as tk
+from tkinter import messagebox
+import joblib
+
 
 # Run the preprocessing script
-subprocess.run(["python", "01-04_Data_preprocessing_Scripts.py"])
+#subprocess.run(["python", "01-04_Data_preprocessing_Scripts.py"])
 
 # Run the PCA + SVM training script
-subprocess.run(["python", "05_pca_svm_training.py"])
+#subprocess.run(["python", "05_pca_svm_training.py"])
+
+#Paths
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+MODEL_PATH = PROJECT_ROOT / "pca_svm_model.joblib"
+
+try:
+    model = joblib.load(MODEL_PATH)
+except Exception as e:
+    raise RuntimeError(f"Model not found or failed to load: {e}")
+
+def dna_feat(sequence, k=3):
+    sequence = sequence.upper()
+    kmers={}
+
+    for i in range(len(sequence)-k+1):
+        kmer=sequence[i:i+k]
+        if kmer not in kmers:
+            kmers[kmer]=0
+        kmers[kmer]+=1
+    
+    all_kmers = sorted(kmers.keys())
+    feature_vec = np.array([kmers[k] for k in all_kmers])
+
+    return feature_vec.reshape(1,-1)
