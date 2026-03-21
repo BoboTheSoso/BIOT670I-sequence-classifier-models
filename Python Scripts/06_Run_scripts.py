b29ae1eb2fb828339228cb2fd13d5b741ccd288f
Solenn Orozco <solinethira@gmail.com>
2026-03-21 17:46:29 -0400

    Update 06_Run_scripts.py
    Change the model loading step into a traing or loading step to train the model if a joblib file does not already exist.

--- Python Scripts/06_Run_scripts.py
+++ Python Scripts/06_Run_scripts.py
@@ -1,6 +1,17 @@
 '''
-This script is only to run the preprocessing script and the pca+svm training script.
-Add a GUI
+GUI script. 
+Name: 06_Run_scripts.py
+
+Purpose:
+GUI script to launch a window and use the pre-trained model from script 05 to predict a sequence inputed by the user.
+Sequence is broken into 3-mers and loaded into the model, then a prediction is made alongside a confidence level.
+
+NEED:
+250bp window check
+-Option to train the model using scripts 01-04 and 05 (in case the joblib does not exist)
+
+
+
 '''
 import os
 import numpy as np
@@ -11,6 +22,7 @@ from tkinter.filedialog import askopenfilename
 import joblib
 import itertools
 from collections import Counter
+
 
 #Paths
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
@@ -23,6 +35,29 @@ try:
     model = joblib.load(MODEL_PATH)
 except Exception as e:
     raise RuntimeError(f"Model not found or failed to load: {e}")
+
+
+#-----------------------------------------------------------
+# Method to train/re-train the model
+#-----------------------------------------------------------
+def load_or_train_model():
+    global model
+    try:
+        model = joblib.load(MODEL_PATH)
+    except:
+        result_label.config(text="No existing model found, initiating pre-processing script. This will take time.")
+        root.update()
+        with open("01-04_Data_preprocessing_Scripts.py") as preprocessing_step:
+            exec(preprocessing_step.read())
+            result_label.config(text="Preprocessing step completed. Initiating model training step.")
+            root.update()
+        with open("05_pca_svm_training.py") as training_step:
+            exec(training_step.read())
+            result_label.config(text="Model training completed, thank you for your patience. Loading model now...")
+            root.update()
+
+        model = joblib.load(MODEL_PATH)
+
 
 #-----------------------------------------------------------
 # Method for kmer vector-ing
@@ -95,6 +130,7 @@ def classify_seq():
         prob = model.predict_proba(features)[0].max()  # max probability for predicted class
         label = "Coding" if prediction == 1 else "Non-coding"
         result_label.config(text=f"File Name: {filename}\nPrediction: {label}\nConfidence: {prob:.3f}")
+        root.update()
         
     except Exception as e:
         messagebox.showerror("Error", str(e))
@@ -108,19 +144,9 @@ root.geometry("500x300")
 root.resizable(False,False)
 label = tk.Label(root, text="DNA Sequence Classifier")
 label.pack(pady=10)
+button2 = tk.Button(root, text = "Train the model (this will take a while...)", command= load_or_train_model)
 button = tk.Button(root, text = "Select input sequence", command= classify_seq)
 button.pack(pady=10)
 result_label = tk.Label(root, text="")
 result_label.pack(pady=20)
 root.mainloop()
-
-
-'''
-Pseudocode:
-
-Get all paths
-GUI pops up to let user select file from their machine
-scripts processes the file: kmer breakage np for model plug-in  
-uses pre-saved model from joblib to give a prediction
-test evaluation grid thing + final prediction coding vs non-coding
-'''
